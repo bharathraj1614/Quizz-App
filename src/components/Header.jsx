1e825768e13a4386582cd1dfbd93d33eccac8cb9
bharathraj <bharathraj1614@gmail.com>
2025-04-16 14:43:34 +0530

    updated logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="./src/assets/react.svg" alt="React logo" />
+      <img
+        src="https://raw.githubusercontent.com/bharathraj1614/Quizz-App/706715f99f3eacfea51d9e2b5e53309f5096343d/src/assets/react.svg"
+        alt="React logo"
+      />
       <h1>The React Quiz</h1>
     </header>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"; // or vue, svelte, etc.
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/bharathraj1614/Quizz-App", // ← replace with your repo name
+  base: "/Quizz-App/", // ← replace with your repo name
 });
